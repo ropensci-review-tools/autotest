6a97b020aadadb35a148d6c3d8c66e17f0b1964b
mpadge <mark.padgham@email.com>
2020-09-15 13:02:34 +0200

    fix parse_all_msgs

--- R/utils.R
+++ R/utils.R
@@ -1,34 +1,46 @@
 
+# A `tryCatch` call modified to catch all messages, warnings, and errors.
+# `tryCatch` can not normally do this because it only catches the first
+# condition. This code is similar to the code of `demo(error.catching)`, but
+# instead of assigning a global variable, it uses a sink to capture all output,
+# and the parses the result with a separate function. See also the `tryCatchLog`
+# package for further inspiration.
+
 catch_all_msgs <- function (f, this_fn) {
     con <- file (f, "wt")
     sink (file = con, type = "message")
-    res <- eval (call (this_fn))
-    closeAllConnections ()
+
+    tryCatch (withCallingHandlers (
+                                   eval (call (this_fn)),
+                                   error=function(e) message (paste0 (e)),
+                                       warning=function(w) {
+                                           message (paste0 (w))
+                                           invokeRestart("muffleWarning")
+                                       }))
+    sink (file = NULL, type = "message")
+    close (con, type = "wt")
 }
 
 parse_all_msgs <- function (f) {
     x <- readLines (f)
+    x <- x [which (!(x == "" | duplicated (x)))]
+
+    # some errors and warnings have different whitespace so:
+    x <- x [which (!duplicated (gsub ("\\s+", "", x)))]
 
     ret <- NULL
 
-    # warnings are always 2 lines; errors and messages only ever 1 line
-    index <- grep ("Warning message:", x)
-    if (length (index) > 0) {
-        index_msg <- index + 1
-        ret <- data.frame (type = rep ("warning", length (index)),
-                           content = x [index_msg],
-                           stringsAsFactors = FALSE)
-        x <- x [-c (index, index_msg)]
-    }
-
-    index <- grep ("^Error", x)
+    index <- grep ("Warning\\:|^Error", x)
     if (length (index) > 0) {
         for (i in index) {
             colon1 <- regexpr ("\\:", x [i])
+            content <- gsub ("^\\s?", "",
+                             substring (x [i], colon1 + 1, nchar (x [i])))
+            type <- c ("warning", "error") [grepl ("^Error", x [i]) + 1]
+
             ret <- rbind (ret,
-                          data.frame (type = "error",
-                                      content = gsub ("^\\s?", "",
-                                                      substring (x [i], colon1 + 1, nchar (x [i]))),
+                          data.frame (type = type,
+                                      content = content,
                                       stringsAsFactors = FALSE)
                           )
         }
