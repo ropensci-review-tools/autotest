7dd7577b7382018e110361dc38480370ddaa35a1
mpadge <mark.padgham@email.com>
2020-09-24 09:34:00 +0200

    append info on undocumented examples to summary method

--- R/methods.R
+++ R/methods.R
@@ -19,6 +19,7 @@ summary.autotest_pkg <- function (x) {
     res [res == 0] <- NA_integer_
 
     message ("autotesting package [", attr (x, "packageName"),
+             ", v", packageVersion (attr (x, "packageName")),
              "] generated ", nrow (x), " rows of output ",
              "of the following types:")
     err_txt <- ifelse (sum (errors) == 1, "", "s")
@@ -29,7 +30,26 @@ summary.autotest_pkg <- function (x) {
     message ("     ", sum (messages), " message", msg_txt)
     diag_txt <- ifelse (sum (diagnostics) == 1, "", "s")
     message ("     ", sum (diagnostics), " other diagnostics", diag_txt)
+    xtrim <- x [which (!grepl ("no documented example", x$content)), ]
+    message ("That corresponds to ",
+             round (nrow (xtrim) / length (unique (xtrim$fn_name)), 3),
+             " messages per documented function (which has examples)")
     message ("")
 
     print (res)
+
+    no_ex <- grep ("no documented example", x$content)
+    if (length (no_ex) > 0) {
+        message ("\nIn addition to the values in that table, the output ",
+                 "includes ", length (no_ex), " functions which have no ",
+                 "documented examples: ")
+        for (i in seq_along (no_ex)) {
+            message ("    ", i, ". ", x$fn_name [no_ex [i]])
+        }
+    }
+
+    if (!is.null (attr (x, "githash"))) {
+        message ("\n    git hash for package as analysed here:\n    [",
+                 attr (x, "githash"), "]")
+    }
 }
