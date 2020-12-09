d15b5d46306880d8244555116e4fc08ed56cacf4
mpadge <mark.padgham@email.com>
2020-12-09 15:33:50 +0100

    pass test param through to test_single_name

--- R/input-name.R
+++ R/input-name.R
@@ -1,24 +1,27 @@
-test_single_name <- function (pkg, this_fn, params, i) {
+test_single_name <- function (pkg, this_fn, params, i, test = TRUE) {
 
-    res <- NULL
-
-    f <- file.path (tempdir (), "junk.txt")
-
-    params_i <- params
     this_class <- class (params [[i]]) [1]
-    params_i [[i]] <- as.character (params_i [[i]])
-    msgs <- catch_all_msgs (f, this_fn, params_i)
-    if (!is.null (msgs)) {
-        operation <- paste0 ("(unquoted) ",
-                             this_class,
-                             " param as (quoted) character")
-        res <- rbind (res,
-                      report_object (type = msgs$type,
-                                     fn_name = msgs$fn_name,
-                                     parameter = names (params) [i],
-                                     parameter_type = "single formula or name",
-                                     operation = operation,
-                                     content = msgs$content))
+    operation <- paste0 ("(unquoted) ",
+                         this_class,
+                         " param as (quoted) character")
+    res <- report_object (type = "dummy",
+                          fn_name = this_fn,
+                          parameter = names (params) [i],
+                          parameter_type = "single formula or name",
+                          operation = operation)
+
+    if (test) {
+
+        f <- tempfile (fileext = ".txt")
+
+        params [[i]] <- as.character (params [[i]])
+        msgs <- catch_all_msgs (f, this_fn, params_i)
+        if (!is.null (msgs)) {
+            res$type <- msgs$type
+            res$content <- msgs$content
+        } else {
+            res <- NULL
+        }
     }
 
     return (res)
--- R/test-single-val.R
+++ R/test-single-val.R
@@ -21,43 +21,30 @@ autotest_single <- function (pkg,
             params_i <- params
 
             p_i <- params_i [[i]]
-            val_type <- param_type <- NULL
+            val_type <- NULL
             check_vec <- TRUE
             if (is_int (p_i)) {
                 val_type <- "integer"
-                param_type <- "single integer"
                 res <- rbind (res,
                               test_single_int (pkg, this_fn, params_i, i, test))
 
             } else if (methods::is (p_i, "numeric")) {
                 val_type <- "numeric"
-                param_type <- "single numeric"
                 res <- rbind (res,
                               test_single_double (this_fn, params_i, i, test))
             } else if (is.character (p_i)) {
                 val_type <- "character"
-                param_type <- "single character"
                 res <- rbind (res,
                               test_single_char (this_fn, params_i, i, test))
             } else if (is.logical (p_i)) {
                 val_type <- "logical"
-                param_type <- "single logical"
                 res <- rbind (res,
                               test_single_logical (this_fn, params_i, i, test))
             } else if (methods::is (p_i, "name") |
                        methods::is (p_i, "formula")) {
                 val_type <- class (p_i) [1]
-                param_type <- paste0 ("single ", val_type)
-                if (test)
-                    res <- rbind (res,
-                                  test_single_name (pkg, this_fn, params_i, i))
-                else
-                    res <- rbind (res,
-                                  report_object (type = "dummy",
-                                                 fn_name = this_fn,
-                                                 parameter = names (params) [i],
-                                                 parameter_type = param_type,
-                                         operation = "single formula tests"))
+                res <- rbind (res,
+                              test_single_name (pkg, this_fn, params_i, i, test))
                 check_vec <- FALSE
             } else {
                 check_vec <- FALSE
