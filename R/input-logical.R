9957c79714f950c8bce146c239856f585528ecfb
mpadge <mark.padgham@email.com>
2020-12-09 14:36:42 +0100

    implement generic subst_for_logical fn

--- R/input-logical.R
+++ R/input-logical.R
@@ -16,42 +16,43 @@ test_single_logical <- function (this_fn, params, i) {
         res_ints <- catch_all_msgs (f, this_fn, params)
     }
     if (is.null (res_ints)) {
-        operation <- "substitute integer values for logical parameter"
-        content <- paste0 ("Parameter ",
-                           names (params) [i],
-                           " of function [",
-                           this_fn,
-                           "] is assumed to be logical, ",
-                           "but responds to ",
-                           "general integer values.")
         res <- rbind (res,
-                      report_object (type = "diagnostic",
-                                     fn_name = this_fn,
-                                     parameter = names (params) [i],
-                                     parameter_type = "single logical",
-                                     operation = operation,
-                                     content = content))
+                      subst_for_logical (this_fn, params, i,
+                                         subst = "integer"))
     }
 
     p [[i]] <- "a"
     res_char <- catch_all_msgs (f, this_fn, p)
     if (null_or_not (res_char, "error")) {
-        operation <- "substitute character values for logical parameter"
-        content <- paste0 ("Parameter ",
-                           names (params) [i],
-                           " of function [",
-                           this_fn,
-                           "] is assumed to be logical, ",
-                           "but responds to ",
-                           "character input")
         res <- rbind (res,
-                      report_object (type = "diagnostic",
-                                     fn_name = this_fn,
-                                     parameter = names (params) [i],
-                                     parameter_type = "single logical",
-                                     operation = operation,
-                                     content = content))
+                      subst_for_logical (this_fn, params, i,
+                                         subst = "character"))
     }
 
     return (res)
+}
+
+#' Construt report object from results of subsituting other kinds of parameters
+#' for assumed logical parameters
+#' @noRd
+subst_for_logical <- function (this_fn, params, i, subst = "integer") {
+
+    operation <- paste0 ("substitute ",
+                         subst,
+                         "values for logical parameter")
+    content <- paste0 ("Parameter ",
+                       names (params) [i],
+                       " of function [",
+                       this_fn,
+                       "] is assumed to be logical, ",
+                       "but responds to ",
+                       subst,
+                       " input")
+
+    return (report_object (type = "diagnostic",
+                           fn_name = this_fn,
+                           parameter = names (params) [i],
+                           parameter_type = "single logical",
+                           operation = operation,
+                           content = content))
 }
