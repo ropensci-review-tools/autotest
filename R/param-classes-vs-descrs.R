aebb372e5e5bd5285ce77bf940560013aad59173
mpadge <mark.padgham@email.com>
2020-12-15 14:41:55 +0100

    seq not seq_along

--- R/param-classes-vs-descrs.R
+++ R/param-classes-vs-descrs.R
@@ -139,7 +139,7 @@ param_classes_in_desc <- function (yaml, pkg_full) {
 
     class_in_desc <- NULL
     if (nrow (these_params) > 0) {
-        class_in_desc <- vapply (seq_along (nrow (these_params)), function (i) {
+        class_in_desc <- vapply (seq (nrow (these_params)), function (i) {
                                      class_i <- these_classes$object
                                      these <- these_params$param [i]
                                      if (these %in% names (param_classes))
