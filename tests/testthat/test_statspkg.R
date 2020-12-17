15716583d1f15583e2c60b5f9ba1b774c1dcc609
mpadge <mark.padgham@email.com>
2020-12-17 11:35:35 +0100

    tweak test_stats

--- tests/testthat/test_statspkg.R
+++ tests/testthat/test_statspkg.R
@@ -12,9 +12,22 @@ test_that("stats", {
 
               is_source <- pkg_is_source (package)
               expect_false (is_source)
-              exs <- get_all_examples (package, is_source, exclude)
 
-              expect_is (exs, "list")
-              expect_equal (length (exs), 1)
-              expect_true (length (exs [[1]]) > 1)
+              fns <- functions
+              topics <- m_fns_to_topics (fns, package)
+              expect_is (topics, "data.frame")
+              expect_equal (ncol (topics), 3)
+
+              topic <- topics$topic
+              expect_length (topic, 1)
+              expect_is (topic, "character")
+              fns <- find_fn_call_points (topic, package)
+              expect_is (fns, "character")
+              expect_true (length (fns) > 1)
+
+              #exs <- get_all_examples (package, is_source, exclude)
+
+              #expect_is (exs, "list")
+              #expect_equal (length (exs), 1)
+              #expect_true (length (exs [[1]]) > 1)
 })
