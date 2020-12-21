040b58411360c498844e1a21b8e2d5f8918f3409
mpadge <mark.padgham@email.com>
2020-12-21 22:39:33 +0100

    rm junk lines that shouldna been in tests

--- tests/testthat/test_statspkg.R
+++ tests/testthat/test_statspkg.R
@@ -13,11 +13,6 @@ test_that("stats", {
               is_source <- pkg_is_source (package)
               expect_false (is_source)
 
-        fns_short <- vapply (fns, function (i) strsplit (i, "\\.") [[1]] [1],
-                             character (1), USE.NAMES = FALSE)
-        expect_is (fns_short, "character")
-        expect_equal (length (fns_short), length (fns))
-
               exs <- get_all_examples (package, is_source, exclude)
 
               expect_is (exs, "list")
