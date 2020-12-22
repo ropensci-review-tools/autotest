483958821f716ebcabcaedffdb874bdfc1eab1bb
mpadge <mark.padgham@email.com>
2020-12-22 11:41:42 +0100

    test stats should now work with options set

--- tests/testthat/test_statspkg.R
+++ tests/testthat/test_statspkg.R
@@ -2,30 +2,26 @@ context("stats-package")
 
 test_that("stats", {
 
-              package <- "stats"
-              functions <- "var"
-              exclude <- NULL
+    package <- "stats"
+    functions <- "var"
+    exclude <- NULL
 
-              exclude <- exclude_functions (package, functions, exclude)
-              expect_is (exclude, "character")
-              expect_true (length (exclude) > 100)
+    exclude <- exclude_functions (package, functions, exclude)
+    expect_is (exclude, "character")
+    expect_true (length (exclude) > 100)
 
-              is_source <- pkg_is_source (package)
-              expect_false (is_source)
+    is_source <- pkg_is_source (package)
+    expect_false (is_source)
 
-              exs <- get_all_examples (package, is_source, exclude)
+    exs <- get_all_examples (package, is_source, exclude)
+    expect_is (exs, "list")
+    expect_length (exs, 1)
+    expect_true (length (exs [[1]]) > 1)
+    expect_length (exs [[1]], 6)
+    expect_length (exs [[1]] [[6]], 4)
 
-              expect_is (exs, "list")
-              expect_equal (length (exs), 1)
-              expect_true (length (exs [[1]]) > 1)
-
-        ex <- examples_to_yaml (package = package, functions = functions)
-        expect_is (ex, "list")
-        expect_true (length (ex) >= 1)
-
-        #x <- autotest (yaml = ex, test = FALSE)
-        #expect_is (x, "tbl_df")
-        #expect_true (all (x$type == "dummy"))
-        #expect_false (all (x$fn_name == functions))
-        #expect_true (all (unique (x$fn_name) %in% c ("cov", "cor", "var")))
+    exs <- examples_to_yaml (package, exclude = exclude)
+    expect_is (exs, "list")
+    expect_length (exs, 4)
+    expect_true (all (names (exs) == "var"))
 })
