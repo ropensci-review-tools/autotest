81bab5eca36c2940217f0e8e97f2e38d6b527923
mpadge <mark.padgham@email.com>
2021-01-14 14:25:03 +0100

    add test_name to autotest_obj for #34

--- R/autotest-class.R
+++ R/autotest-class.R
@@ -3,6 +3,8 @@
 #' @param package Name of package for which object is to be constructed.
 #' @param package_loc Location of package on local system (for source packages
 #' only)
+#' @param test_name Name of test (use \link{autotest_types} to get all test
+#' names).
 #' @param fn_name Name of function to be tested.
 #' @param parameters Names of all parameters for that function.
 #' @param parameter_types Types of input parameters.
@@ -15,19 +17,21 @@
 #' object.
 #'
 #' @export
-autotest_obj <- function (package,
+autotest_obj <- function (package = NA_character_,
                           package_loc = NULL,
-                          fn_name,
-                          parameters,
-                          parameter_types,
+                          test_name = NA_character_,
+                          fn_name = NA_character_,
+                          parameters = list (),
+                          parameter_types = NA_character_,
                           class = NULL,
-                          classes,
+                          classes = NULL,
                           env = new.env (),
                           test = FALSE,
                           quiet = FALSE) {
 
     x <- list (package = package,
                package_loc = package_loc,
+               test_name = test_name,
                params = parameters,
                param_types = parameter_types,
                fn = fn_name,
