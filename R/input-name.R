9ad9edaa0ce60c71c108efe2fe8825f1f48fa05d
mpadge <mark.padgham@email.com>
2021-01-14 14:00:51 +0100

    add test_name to input-name for #35

--- R/input-name.R
+++ R/input-name.R
@@ -5,6 +5,7 @@ test_single_name <- function (x = NULL, ...) {
 
 test_single_name.NULL <- function (x = NULL, ...) {
     report_object (type = "dummy",
+                   test_name = "name_or_formula_as_char",
                    parameter_type = "single name or formula",
                    operation = "Unquoted name/formula as quoted character",
                    content = "Capture any warnings or errors issued")
@@ -12,15 +13,14 @@ test_single_name.NULL <- function (x = NULL, ...) {
 
 test_single_name.autotest_obj <- function (x) {
 
-    this_class <- class (x$params [[x$i]]) [1]
-    operation <- paste0 ("(unquoted) ",
-                         this_class,
-                         " param as (quoted) character")
-    res <- report_object (type = "dummy",
-                          fn_name = x$fn,
-                          parameter = names (x$params) [x$i],
-                          parameter_type = "single formula or name",
-                          operation = operation)
+    ret <- test_single_name.NULL ()
+
+    ret$fn_name <- x$fn
+    ret$parameter <- names (x$params) [x$i]
+
+    ret$operation <- paste0 ("(unquoted) ",
+                             class (x$params [[x$i]]) [1],
+                             " param as (quoted) character")
 
     if (x$test) {
 
