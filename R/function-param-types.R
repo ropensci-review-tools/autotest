fde26d4eb14d4bf076c0909239faff251d1023d7
mpadge <mark.padgham@email.com>
2026-01-27 10:41:46 +0100

    spaceout R/function-param-types.R

--- R/function-param-types.R
+++ R/function-param-types.R
@@ -1,4 +1,3 @@
-
 get_param_types <- function (fn, params, par_lengths) {
 
     if (any (params == "NULL")) {
@@ -27,54 +26,67 @@ get_param_types <- function (fn, params, par_lengths) {
 
 single_params <- function (params) {
 
-        is_single <- function (j) {
-            chk <- FALSE
-            if (is.null (dim (j)) && length (j) == 1) {
-                if (methods::is (j, "name")) {
-                    val <- tryCatch (eval (parse (text = j)),
-                                     error = function (e) NULL)
-                    if (!is.null (val))
-                        chk <- length (val) == 1
-                } else if (!isS4 (j)) {
-                    # single objects can still be almost anything, so only
-                    # consider as truly single those objects which have
-                    # attribute lists each element of which have at most two
-                    # elements. This is entirely arbitrary, and should be
-                    # modified once more is known about the kinds of things
-                    # thrown at this function.
-                    lens <- vapply (attributes (j), length, integer (1))
-                    chk <- !any (lens > 2)
+    is_single <- function (j) {
+        chk <- FALSE
+        if (is.null (dim (j)) && length (j) == 1) {
+            if (methods::is (j, "name")) {
+                val <- tryCatch (eval (parse (text = j)),
+                    error = function (e) NULL
+                )
+                if (!is.null (val)) {
+                    chk <- length (val) == 1
                 }
-            } else if (methods::is (j, "formula")) {
-                chk <- TRUE
+            } else if (!isS4 (j)) {
+                # single objects can still be almost anything, so only
+                # consider as truly single those objects which have
+                # attribute lists each element of which have at most two
+                # elements. This is entirely arbitrary, and should be
+                # modified once more is known about the kinds of things
+                # thrown at this function.
+                lens <- vapply (attributes (j), length, integer (1))
+                chk <- !any (lens > 2)
             }
-            return (chk)
+        } else if (methods::is (j, "formula")) {
+            chk <- TRUE
         }
+        return (chk)
+    }
 
-    return (which (vapply (params, function (j)
-                           is_single (j),
-                           logical (1))))
+    return (which (vapply (
+        params, function (j) {
+            is_single (j)
+        },
+        logical (1)
+    )))
 }
 
 vector_params <- function (params) {
 
-    return (which (vapply (params, function (i)
-                           length (i) > 1 &&
-                               is.null (dim (i)) &&
-                               is.atomic (i) &&
-                               length (class (i) <= 1L) &&
-                               any (grepl (atomic_modes (collapse = TRUE),
-                                           class (i))),
-                           logical (1))))
+    return (which (vapply (
+        params, function (i) {
+            length (i) > 1 &&
+                is.null (dim (i)) &&
+                is.atomic (i) &&
+                length (class (i) <= 1L) &&
+                any (grepl (
+                    atomic_modes (collapse = TRUE),
+                    class (i)
+                ))
+        },
+        logical (1)
+    )))
 }
 
 tabular_params <- function (params) {
 
-    return (which (vapply (params, function (i)
-                           length (dim (i)) == 2 &
-                               !(inherits (i, "Matrix") |
-                                 inherits (i, "matrix")),
-                           logical (1))))
+    return (which (vapply (
+        params, function (i) {
+            length (dim (i)) == 2 &
+                !(inherits (i, "Matrix") |
+                    inherits (i, "matrix"))
+        },
+        logical (1)
+    )))
 }
 
 #' single_or_vec
@@ -94,36 +106,48 @@ single_or_vec <- function (res) {
 
         pars <- res$parameters [names (res$parameters) == f]
         pars <- lapply (pars, function (i) {
-                    nms <- names (unlist (i))
-                    lens <- vapply (nms, function (j) {
-                                ij <- unlist (i) [[j]]
-                                out <- length (ij)
-                                if (methods::is (ij, "name")) {
-                                    tmp <- tryCatch (
-                                            eval (parse (text = ij),
-                                                  envir = pkg_env),
-                                            error = function (e) NULL)
-                                    if (!is.null (tmp))
-                                        out <- length (tmp)
-                                }
-                                return (out)
-                            },
-                            integer (1))
-                    data.frame (name = nms,
-                                len = lens) })
+            nms <- names (unlist (i))
+            lens <- vapply (
+                nms, function (j) {
+                    ij <- unlist (i) [[j]]
+                    out <- length (ij)
+                    if (methods::is (ij, "name")) {
+                        tmp <- tryCatch (
+                            eval (parse (text = ij),
+                                envir = pkg_env
+                            ),
+                            error = function (e) NULL
+                        )
+                        if (!is.null (tmp)) {
+                            out <- length (tmp)
+                        }
+                    }
+                    return (out)
+                },
+                integer (1)
+            )
+            data.frame (
+                name = nms,
+                len = lens
+            ) })
 
         pars <- data.frame (do.call (rbind, unname (pars)))
-        pars <- lapply (split (pars, f = as.factor (pars$name)),
-                        function (i)
-                            i [which.max (i$len), , drop = FALSE])
+        pars <- lapply (
+            split (pars, f = as.factor (pars$name)),
+            function (i) {
+                i [which.max (i$len), , drop = FALSE]
+            }
+        )
 
         pars <- do.call (rbind, pars)
 
-        data.frame (fn = f,
-                    par = pars$name,
-                    single = pars$len == 1,
-                    stringsAsFactors = FALSE)
-                     })
+        data.frame (
+            fn = f,
+            par = pars$name,
+            single = pars$len == 1,
+            stringsAsFactors = FALSE
+        )
+    })
 
     return (do.call (rbind, pars))
 }
@@ -141,11 +165,14 @@ double_or_int <- function (res) {
 
     is_par_int <- function (p) {
         ret <- FALSE
-        if (is.numeric (p))
+        if (is.numeric (p)) {
             ret <- all (abs (p - round (p)) < .Machine$double.eps)
-        if (!is.null (attr (p, "is_int")))
-            if (!attr (p, "is_int"))
+        }
+        if (!is.null (attr (p, "is_int"))) {
+            if (!attr (p, "is_int")) {
                 ret <- FALSE
+            }
+        }
         return (ret)
     }
 
@@ -157,28 +184,34 @@ double_or_int <- function (res) {
         names (pars) <- nms
 
         pars <- lapply (seq_along (pars), function (i) {
-                            nms <- names (pars) [i]
-                            int_val <- is_par_int (pars [[i]])
-                            data.frame (name = nms,
-                                        int_val = int_val)
-                    })
+            nms <- names (pars) [i]
+            int_val <- is_par_int (pars [[i]])
+            data.frame (
+                name = nms,
+                int_val = int_val
+            )
+        })
 
         pars <- data.frame (do.call (rbind, unname (pars)))
-        pars <- lapply (split (pars, f = as.factor (pars$name)),
-                        function (i) {
-                            int_val <- all (i$int_val)
-                            i <- i [1, ]
-                            i$int_val <- int_val
-                            return (i)
-                        })
+        pars <- lapply (
+            split (pars, f = as.factor (pars$name)),
+            function (i) {
+                int_val <- all (i$int_val)
+                i <- i [1, ]
+                i$int_val <- int_val
+                return (i)
+            }
+        )
 
         pars <- do.call (rbind, pars)
 
-        data.frame (fn = f,
-                    par = pars$name,
-                    int_val = pars$int_val,
-                    stringsAsFactors = FALSE)
-                     })
+        data.frame (
+            fn = f,
+            par = pars$name,
+            int_val = pars$int_val,
+            stringsAsFactors = FALSE
+        )
+    })
 
     return (do.call (rbind, pars))
 }
@@ -191,8 +224,9 @@ add_int_attrs <- function (x, int_val) {
 
     if (nrow (int_val) > 0) {
         for (p in int_val$par) {
-            if (is.numeric (x$params [[p]]))
+            if (is.numeric (x$params [[p]])) {
                 attr (x$params [[p]], "is_int") <- TRUE
+            }
         }
     }
 
