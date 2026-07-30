2a14b62dc6e107c926b8cb1ab7acea4eaa08bcc9
mpadge <mark.padgham@email.com>
2026-07-30 11:29:38 +0200

    002: complete get_params/scrape-examples.R cleanup
    Trim R/rd-lookup.R down to the live Rd-lookup helpers, relocate the
    still-live get_package_name() into R/namespace-processing.R, drop the
    now-unreachable is_pkg_same()/topic_to_fns()/m_topic_to_fns(), restore
    preload_package() (a live autotest_package() dependency that a prior
    cleanup pass had deleted along with R/examples-to-yaml.R), and remove
    the corresponding dead test block. Completes the previous commit, which
    staged the git-mv/git-rm operations before these content edits.

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,48 @@
 get_Rd_metadata <- utils::getFromNamespace (".Rd_get_metadata", "tools") # nolint
 
+#' Load a package (installed or local source) into the current session, and
+#' return its name.
+#' @param package Name of locally installed package or path to local source
+#' @return Name of the package
+#' @noRd
+preload_package <- function (package) {
+
+    if (pkg_is_source (package)) {
+
+        pkg_name <- get_package_name (package)
+        if (!paste0 ("package:", pkg_name) %in% search ()) {
+            requireNamespace ("devtools")
+            devtools::load_all (package, export_all = FALSE)
+        }
+
+    } else if (!basename (package) == package) {
+
+        # pkgs installed in tmp_loc via covr
+        suppressMessages (
+            library (basename (package),
+                lib.loc = normalizePath (file.path (package, "..")),
+                character.only = TRUE
+            )
+        )
+        pkg_name <- basename (package)
+
+    } else {
+
+        ip <- data.frame (utils::installed.packages (),
+            stringsAsFactors = FALSE
+        )
+        if (!package %in% ip$Package) {
+            stop ("package [", package, "] does not appear to be installed.")
+        }
+        suppressMessages (
+            library (package, character.only = TRUE)
+        )
+        pkg_name <- package
+    }
+
+    return (pkg_name)
+}
+
 rm_internal_namespace <- function (x) {
 
     regmatches (
--- tests/testthat/test-statspkg.R
+++ tests/testthat/test-statspkg.R
@@ -1,26 +1,5 @@
 context ("stats-package")
 
-test_that ("get-examples", {
-
-    package <- "stats"
-    functions <- "var"
-    exclude <- NULL
-
-    exclude <- exclude_functions (package, functions, exclude)
-    expect_is (exclude, "character")
-    expect_true (length (exclude) > 100)
-
-    is_source <- pkg_is_source (package)
-    expect_false (is_source)
-
-    exs <- get_all_examples (package, is_source, exclude)
-    expect_is (exs, "list")
-    expect_length (exs, 1)
-    expect_true (length (exs [[1]]) > 1)
-    expect_length (exs [[1]], 6)
-    expect_length (exs [[1]] [[6]], 4)
-})
-
 test_that ("autotest var", {
 
     package <- "stats"
